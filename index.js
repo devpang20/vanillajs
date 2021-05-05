5a3f044b9db40d5a951810ce665937bd7d7264a9
sik2 <devpang20@gmail.com>
2021-05-06 01:14:45 +0900

    feat:color change when dom clicked

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const titleSelect = document.querySelector("#title");
 
-function handleCLick() {
-    title.style.color = "blue";
+
+const BASE_COLOR = "rgb(52, 73, 94)"; // rgb(52, 73, 94)
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+    const currentColor = titleSelect.style.color;
+    console.log(currentColor);
+    if (currentColor === BASE_COLOR) {
+        titleSelect.style.color = OTHER_COLOR;
+    } else {
+        titleSelect.style.color = BASE_COLOR;
+    }
+
 }
 
-titleSelect.addEventListener("click", handleCLick);
+function init() {
+    titleSelect.style.color = BASE_COLOR;
+    titleSelect.addEventListener("click", handleClick); //always HTML Javascript DOM event MDN 검색을 통해 이벤트 참조하기
+}
+
+init();
+
 
 

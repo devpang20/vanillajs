525eb5e8083868fd6ea0ee73af8c67f8e42c85d9
sik2 <devpang20@gmail.com>
2021-05-05 17:42:42 +0900

    test:event handler

---
+++ index.js
@@ -0,0 +1,7 @@
+const titieId = document.getElementId("title");
+
+const titleSelect = document.querySelector("#title");
+
+titieId .innerHTML = "change HTML";
+titleSelect.style.color = 'red';
+document.titleSelect = 'test'

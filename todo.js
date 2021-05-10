bd6d48cfdc3814d6b8c8c160457f5aadd8ee265b
sik2 <devpang20@gmail.com>
2021-05-10 23:10:53 +0900

    feat:배열 객체 생성

--- todo.js
+++ todo.js
@@ -4,6 +4,8 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
@@ -13,6 +15,10 @@ function paintToDo(text) {
     li.appendChild(delBtn);
     li.appendChild(span);
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id:toDos.length + 1,
+    }
 }
 
 function handleSubmit(e) {

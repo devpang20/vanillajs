62934aef52976a799ce3c5384b2c2c48223a9685
sik2 <devpang20@gmail.com>
2021-05-12 17:57:44 +0900

    feat:todo작업

--- todo.js
+++ todo.js
@@ -6,19 +6,28 @@ const TODOS_LS = 'toDos';
 
 const toDos = [];
 
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    toDoInput.innerText = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    toDoInput.innerText = "❌";
+
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
         text: text,
-        id:toDos.length + 1,
-    }
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(e) {
@@ -28,9 +37,12 @@ function handleSubmit(e) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
     } else {
 
     }
